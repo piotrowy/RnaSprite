49fdde5f5c697583a5d7fa8482d11253a337af80
Tomasz Żok <Tomasz.Zok@cs.put.poznan.pl>
2016-03-15 13:58:43 +0100

    Test with valid PDB but non-RNA

--- src/test/java/AppControllerTest.java
+++ src/test/java/AppControllerTest.java
@@ -2,15 +2,22 @@ import org.junit.Before;
 import org.junit.Test;
 import pl.put.poznan.AppController;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.junit.Assert.assertNotNull;
+
 /**
  * Created by tzok on 01.03.16.
  */
 public class AppControllerTest {
     private AppController appController;
+    private InputStream pdb1KTR;
 
     @Before
-    public void initialize() {
+    public void initialize() throws IOException {
         appController = new AppController();
+        pdb1KTR = AppControllerTest.class.getResourceAsStream("/1KTR-M.pdb");
     }
 
     @Test
@@ -21,5 +28,10 @@ public class AppControllerTest {
     @Test
     public void testGetAnglesFromPDBInvalidPDBId() {
         appController.getAnglesFromPDB("qwerty");
+    }
+
+    @Test
+    public void testGetAnglesFromFileProtein() throws IOException {
+        assertNotNull(appController.getAnglesFromFile(pdb1KTR, null));
     }
 }
