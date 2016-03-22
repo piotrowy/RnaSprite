479db6f0ac50dde6441b2ce2498336254bbc1e07
piotrowy <petr.ceranek@gmail.com>
2016-03-22 15:31:52 +0100

    Refactor DistanceMatrix.java

--- src/main/java/pl/put/poznan/DistanceMatrix.java
+++ src/main/java/pl/put/poznan/DistanceMatrix.java
@@ -9,6 +9,8 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
@@ -37,48 +39,46 @@ public class DistanceMatrix extends Matrix {
 		this.matrix = this.generateDistanceMatrix(strC, chainIdentifier, at1, at2);
 	}
 
-	private List<List<? extends Row>> generateDistanceMatrix(StructureContainer strC,
-			String chainIdentifier, String at1, String at2) {
-		if (!strC.getStructureList().isEmpty()) {
-			int index = 0;
+    private List<List<? extends Row>> generateDistanceMatrix(StructureContainer strC,
+                                                             String chainIdentifier, String at1, String at2) {
+        if (!strC.getStructureList().isEmpty()) {
+            return strC.getStructureList().stream().map(model -> calculateDistancesInChains(model,
+                    model.getModelNumber(), chainIdentifier, at1, at2)).collect(Collectors.toList());
+        } else {
+            return Collections.emptyList();
+        }
+    }
 
-			List<List<? extends Row>> mtx = new ArrayList<>();
-			for (PdbModel model : strC.getStructureList()) {
-				index++;
-				List<DistanceMatrixRow> distanceMatrixList = new ArrayList<>();
-				for (PdbChain chain : model.getChains()) {
-					if (chain.toString().equalsIgnoreCase(chainIdentifier)) {
-						PdbCompactFragment fragment = pdbChainToCompactFragment(chain);
-						for (PdbResidue residue : fragment.getResidues()) {
-							List<String> r = new ArrayList<>();
-							for (PdbResidue residue1 : fragment.getResidues()) {
-								if (residue.hasAtom(AtomName.fromString(at1))
-										&& residue1.hasAtom(AtomName
-												.fromString(at2))) {
-									DecimalFormat df2 = new DecimalFormat(
-											"#,###,###,##0.00");
-									r.add(df2.format(residue.findAtom(
-											AtomName.fromString(at1))
-											.distanceTo(
-													residue1.findAtom(AtomName
-															.fromString(at2))))
-											+ "");
-								} else {
-									r.add("");
-								}
-							}
-							DistanceMatrixRow row = new DistanceMatrixRow(r,
-									residue.getOneLetterName() + ""
-											+ residue.getResidueNumber(),
-									index, chain.toString() + "");
-							distanceMatrixList.add(row);
-						}
-					}
-				}
-				mtx.add(distanceMatrixList);
-			}
-			return mtx;
-		}
-		return Collections.emptyList();
+    private List<DistanceMatrixRow> calculateDistancesInChains(PdbModel model, int index, String chainIdentifier,
+                                                                         String at1, String at2){
+         PdbChain chain = model.getChains().stream().filter(ch -> ch.toString().equalsIgnoreCase(
+                 chainIdentifier)).findFirst().get();
+         return calculateDistancesBetweenResiduesOuter(index, chain.toString(),
+                 pdbChainToCompactFragment(chain), at1, at2);
+    }
+
+	private List<DistanceMatrixRow> calculateDistancesBetweenResiduesOuter (int index, String chain,
+                                                                            PdbCompactFragment fragment,
+																			String at1, String at2) {
+		return fragment.getResidues().stream().map(res -> new DistanceMatrixRow(
+				calculateDistancesBetweenResiduesInner(fragment, res, at1, at2),
+				res.getOneLetterName() + "" + res.getResidueNumber(), index, chain)).collect(Collectors.toList());
+	}
+
+	private List<String> calculateDistancesBetweenResiduesInner (PdbCompactFragment fragment, PdbResidue residue,
+															String at1, String at2) {
+		List <String> listOfDistancesBetweenResidues = new ArrayList<>();
+		fragment.getResidues().forEach(res -> listOfDistancesBetweenResidues.add(
+				calculateDistance(residue, res, at1, at2)));
+		return listOfDistancesBetweenResidues;
+	}
+
+	private String calculateDistance (PdbResidue residue, PdbResidue residue1, String at1, String at2){
+		DecimalFormat df2 = new DecimalFormat("#,###,###,##0.00");
+		return residue1.hasAtom(AtomName.fromString(at2)) && residue.hasAtom(AtomName.fromString(at1))? df2.format(
+				residue.findAtom(AtomName.fromString(at1)).distanceTo(residue1.findAtom(
+						AtomName.fromString(at2)))) + "" : "";
 	}
 }
+
+
