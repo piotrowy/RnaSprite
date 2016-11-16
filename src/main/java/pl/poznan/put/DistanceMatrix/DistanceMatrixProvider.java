bdb41c6401a3509bef289624a3be4e315801c317
piotrowy <petr.ceranek@gmail.com>
2016-11-16 02:54:02 +0100

    HotFix to distancesMatrix

--- src/main/java/pl/poznan/put/DistanceMatrix/DistanceMatrixProvider.java
+++ src/main/java/pl/poznan/put/DistanceMatrix/DistanceMatrixProvider.java
@@ -1,13 +1,21 @@
 package pl.poznan.put.DistanceMatrix;
 
+import pl.poznan.put.RnaMatrix.RnaMatrix;
 import pl.poznan.put.RnaMatrix.RnaMatrixProvider;
+import pl.poznan.put.Structure.PdbStructure;
 
 import javax.inject.Inject;
+import java.util.List;
 
 public class DistanceMatrixProvider extends RnaMatrixProvider {
 
     @Inject
     public DistanceMatrixProvider(DistancesCalculation torsionAnglesMatrixCalculation) {
         super.calculationMethod = torsionAnglesMatrixCalculation;
+    }
+
+    @Override
+    public List<RnaMatrix> get(PdbStructure obj) {
+        return null;
     }
 }
--- src/main/java/pl/poznan/put/DistanceMatrix/DistancesCalculation.java
+++ src/main/java/pl/poznan/put/DistanceMatrix/DistancesCalculation.java
@@ -1,13 +1,19 @@
 package pl.poznan.put.DistanceMatrix;
 
+import pl.poznan.put.RnaMatrix.MatrixCalculable;
 import pl.poznan.put.Util.Calculable;
 import pl.poznan.put.RnaMatrix.RnaMatrix;
 import pl.poznan.put.Structure.PdbStructure;
+import pl.poznan.put.pdb.analysis.PdbModel;
 
-public class DistancesCalculation implements Calculable {
+public class DistancesCalculation implements MatrixCalculable {
 
+    /**
+     * @param model
+     * @return some derivative matrix.
+     */
     @Override
-    public final RnaMatrix calculateMatrix(final PdbStructure structure) {
+    public RnaMatrix calculateMatrix(PdbModel model) {
         return null;
     }
 }
